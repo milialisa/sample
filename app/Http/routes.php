9be76b579f866865ede003bf9f6e5e753c14d96c
vagrant <vagrant@homestead>
2017-05-10 04:11:17 +0000

    Finish layout and routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,9 @@
 //     return view('welcome');
 // });
 
+get('/', 'StaticPagesController@home')->name('home');
+get('/help', 'StaticPagesController@help')->name('help');
+get('/about', 'StaticPagesController@about')->name('about');
 
-get('/', 'StaticPagesController@home');
-get('/help', 'StaticPagesController@help');
-get('/about', 'StaticPagesController@about');
+
+get('signup', 'UsersController@create')->name('signup');
